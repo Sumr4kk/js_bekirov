a8dcaa2b3ec6943ac7c266f8ef1f433d6d0c0723
SumrakkNote <SumrakkNote>
2024-11-06 10:35:12 +0300

    18-ая задача

--- 1-100/11-20/script_11_20.js
+++ 1-100/11-20/script_11_20.js
@@ -32,3 +32,27 @@ alert(a);
 // 16 задача
 let a_16 = 1 + 2 + 3;
 alert(a_16);
+
+// 18 задача
+let a_18_1 = 10;
+let b_18_1 = 2;
+alert(a_18_1 + b_18_1);
+alert(a_18_1 - b_18_1);
+alert(a_18_1 * b_18_1);
+alert(a_18_1 / b_18_1);
+
+let c_18_2 = 10;
+let d_18_2 = 5;
+let result_18_2 = c_18_2 + d_18_2;
+alert(result_18_2);
+
+let a_18_3 = 1;
+let b_18_3 = 2;
+let c_18_3 = 3;
+alert(a_18_3 + b_18_3 + c_18_3);
+
+let a_18_4 = 10, b_18_4 = 5;
+let c_18_4 = a_18_4 - b_18_4;
+let d_18_4 = 7;
+let result_18_4 = c_18_4 + d_18_4;
+alert(result_18_4);
