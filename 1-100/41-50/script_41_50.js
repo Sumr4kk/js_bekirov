ac7bb325eb7041d960c05101b6a90d122451b92d
Sumr4kk <alimbeckir@yandex.ru>
2024-11-12 13:54:41 +0300

    42-ая задача

--- 1-100/41-50/script_41_50.js
+++ 1-100/41-50/script_41_50.js
@@ -1,2 +1,8 @@
 // 41 задача
-alert(eee); // ошибка во 2 строке кода, переменная не найдена
+ // alert(eee); ошибка во 2 строке кода, переменная не найдена
+
+// 42 задача
+const PI = 3.14;
+const radius = 5;
+const circumference = 2 * PI * radius;
+console.log("Длина окружности с радиусом", radius, "равна", circumference);
