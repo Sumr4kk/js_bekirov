6bee472d6d6c3c8f93ae4f18318627cd348b2da7
Sumr4kk <alimbeckir@yandex.ru>
2024-11-12 13:49:09 +0300

    40-ая задача

--- 1-100/31-40/script_31_40.js
+++ 1-100/31-40/script_31_40.js
@@ -38,3 +38,9 @@ console.log(num);
 
 let num1 = 1, num2 = 2, num3 = 3;
 console.log(num1, num2, num3);
+
+// 40 задача
+console.log(123);
+console.log('123');
+console.log(true);
+console.log(null);
